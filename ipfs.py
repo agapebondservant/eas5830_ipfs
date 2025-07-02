8fd30d9d54487e50254bc46445f3cf7ea89f83e2
agapebondservant <agapebondservant@yahoo.com>
2025-07-02 03:36:49 +0000

    updates

---
+++ ipfs.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+api_key='af83c96cc0ff485bb901f9ed92726df3'
+api_secret='p0xwRkYKBQcRfhM5bQgW6/X70Y5CmcHVAn8QzjZ5jbqEO8r3/xyEjg'
+endpoint = "https://ipfs.infura.io:5001"
+
+def pin_to_ipfs(data):
+    assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+    #YOUR CODE HERE
+    with open('output.json', 'w') as f:
+    json.dump(data, f, indent=4)
+        response = requests.post(endpoint + "/api/v0/add", files={"file":'output.json'}, auth=(api_key, api_secret))
+        cid = response.text.split(",")[1].split(":")[1].replace('"','')
+        return cid
+
+def get_from_ipfs(cid,content_type="json"):
+    assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+    #YOUR CODE HERE	
+    url = f"{endpoint}/api/v0/cat?arg={cid}"
+    response = requests.post(url, params={"arg": cid}, auth=(api_key, api_secret))
+    data = response.json()
+    assert isinstance(data,dict), f"get_from_ipfs should return a dict"
+    return data
