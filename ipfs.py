75c38fa8f6f68b52373fd43c21fabe61513a0299
agapebondservant <agapebondservant@yahoo.com>
2025-07-02 08:57:27 +0000

    updates

--- ipfs.py
+++ ipfs.py
@@ -10,9 +10,9 @@ def pin_to_ipfs(data):
     assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
     #YOUR CODE HERE
     file_name=str(uuid.uuid4())
-    # with open(file_name, 'w') as f:
-    #     json.dump(data, f, indent=4)
-    response = requests.post(endpoint + "/api/v0/add", files={"file":file_name}, auth=(api_key, api_secret))
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4)
+    response = requests.post(f"{endpoint}/api/v0/add", files={"file":f}, auth=(api_key, api_secret))
     with open('output.json', 'w') as f2:
         f2.write(response.text)
     cid = response.text.split(",")[1].split(":")[1].replace('"','')
