7dbd0628209a1b5a9b267349376210f6bc604222
agapebondservant <agapebondservant@yahoo.com>
2025-07-02 07:31:48 +0000

    updates

--- ipfs.py
+++ ipfs.py
@@ -13,9 +13,9 @@ def pin_to_ipfs(data):
     with open(file_name, 'w') as f:
         json.dump(data, f, indent=4)
         response = requests.post(endpoint + "/api/v0/add", files={"file":file_name}, auth=(api_key, api_secret))
-        cid = response.text.split(",")[1].split(":")[1].replace('"','')
         with open('output.json', 'w') as f2:
             f2.write(response.text)
+        cid = response.text.split(",")[1].split(":")[1].replace('"','')
         return cid
 
 def get_from_ipfs(cid,content_type="json"):
