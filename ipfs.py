dd6828b38f0a1967a2e7b765c62a4a15686b0499
agapebondservant <agapebondservant@yahoo.com>
2025-07-03 04:27:40 +0000

    updates

--- ipfs.py
+++ ipfs.py
@@ -19,9 +19,9 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid,content_type="json"):
     assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
     #YOUR CODE HERE	
-    url = f"https://{gateway}/ipfs/{cid}"
-    # url = f"https://gateway.pinata.cloud/ipfs/{cid}"
-    response = requests.get(url, auth=(api_key, api_secret), headers={"Authorization": f"Bearer {jwt}"})
+    # url = f"https://{gateway}/ipfs/{cid}"
+    url = f"https://api.pinata.cloud/data/pinList"
+    response = requests.get(url, headers={"pinata_api_key": api_key, "pinata_secret_api_key": api_secret})
     with open(f'debug{cid}.txt', 'a+') as f2:
         f2.write(response.text)
     data = response.json()['data']
