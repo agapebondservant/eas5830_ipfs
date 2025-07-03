52c7d2f9f56032af8ebec81fc76336bc24827e88
agapebondservant <agapebondservant@yahoo.com>
2025-07-03 08:20:46 +0000

    updates

--- ipfs.py
+++ ipfs.py
@@ -9,12 +9,6 @@ gateway_key = 'a0-eUDrd0ZVRRL4TTOUlfXruyD4_WgjN8H358NowODwGE5GJKvsmAI7gTux1UJps'
 
 def pin_to_ipfs(data):
     assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
-    #YOUR CODE HERE
-    # with open('output.txt', 'w+') as f:
-    #     json.dump(data, f)
-    #     response = requests.post(f"https://uploads.pinata.cloud/v3/files", files={'file':'output.txt'}, headers={"Authorization": f"Bearer {jwt}"})
-    #     cid = response.json()['data']['cid']
-    #     return cid
     url = 'https://api.pinata.cloud/pinning/pinJSONToIPFS'
     body = {"pinataOptions": {"cidVersion": 1}, "pinataContent": data}
     response = requests.post(url, json=body, headers={"Authorization": f"Bearer {jwt}"})
@@ -26,11 +20,7 @@ def get_from_ipfs(cid,content_type="json"):
     assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
     #YOUR CODE HERE	
     url = f"https://{gateway}/ipfs/{cid}"
-    # url = f"https://api.pinata.cloud/data/pinList"
-    # response = requests.get(url, headers={"cid":cid, "pinata_api_key": api_key, "pinata_secret_api_key": api_secret})
     response = requests.get(url, headers={"cid":cid, "Authorization": f"Bearer {jwt}"})
-    with open(f'debug{cid}.txt', 'a+') as f2:
-        f2.write(response.text)
     data = response.json()
     assert isinstance(data,dict), f"get_from_ipfs should return a dict"
     return data
