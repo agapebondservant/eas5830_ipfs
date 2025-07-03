427b329d520eec60c49a491a0424df7546f1023c
agapebondservant <agapebondservant@yahoo.com>
2025-07-03 02:59:53 +0000

    updates

--- ipfs.py
+++ ipfs.py
@@ -21,7 +21,7 @@ def get_from_ipfs(cid,content_type="json"):
     assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
     #YOUR CODE HERE	
     url = f"https://{gateway}/ipfs/${cid}"
-    response = requests.get(url, params={"arg": cid}, headers={"Authorization": f"Bearer {jwt}"})
+    response = requests.get(url, headers={"Authorization": f"Bearer {jwt}"})
     data = response.json()
     assert isinstance(data,dict), f"get_from_ipfs should return a dict"
     return data
