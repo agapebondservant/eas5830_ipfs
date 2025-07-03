d2be54cf2b697e2661fa17fbc4d860f877020da1
agapebondservant <agapebondservant@yahoo.com>
2025-07-03 04:28:36 +0000

    updates

--- ipfs.py
+++ ipfs.py
@@ -21,7 +21,7 @@ def get_from_ipfs(cid,content_type="json"):
     #YOUR CODE HERE	
     # url = f"https://{gateway}/ipfs/{cid}"
     url = f"https://api.pinata.cloud/data/pinList"
-    response = requests.get(url, headers={"pinata_api_key": api_key, "pinata_secret_api_key": api_secret})
+    response = requests.get(url, headers={"cid":cid, "pinata_api_key": api_key, "pinata_secret_api_key": api_secret})
     with open(f'debug{cid}.txt', 'a+') as f2:
         f2.write(response.text)
     data = response.json()['data']
